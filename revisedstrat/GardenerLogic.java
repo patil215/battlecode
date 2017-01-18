f65bc5014b3e5d7fdda3fe81efd8184e9859bda7
patil215 <neilpatil215@gmail.com>
2017-01-17 21:48:29 -0600

    Refactor GardenerLogic to be more readable

--- revisedstrat/GardenerLogic.java
+++ revisedstrat/GardenerLogic.java
@@ -1,7 +1,6 @@
 package revisedstrat;
 
 import battlecode.common.*;
-import revisedstrat.BroadcastManager.LocationInfoType;
 import revisedstrat.BroadcastManager.UnitCountInfoType;
 
 /**
@@ -10,16 +9,20 @@ import revisedstrat.BroadcastManager.UnitCountInfoType;
 public class GardenerLogic extends RobotLogic {
 
 	private Direction moveDir;
-	private int numRoundsSettling;
-	private boolean settled;
-	private boolean towardsCenter;
+	private final int NUM_ROUNDS_TO_SETTLE = 30;
+	private final double TANK_SPAWNER_CHANCE = 0.5;
+
+	private final Direction ENEMY_BASE_OPPOSITE_DIRECTION;
+	private final boolean SHOULD_SPAWN_TANKS;
+	private final int MIN_FREE_SPACE_REQUIREMENT = 4;
 
 	public GardenerLogic(RobotController rc) {
 		super(rc);
-		numRoundsSettling = 0;
+		ENEMY_BASE_OPPOSITE_DIRECTION = rc.getLocation()
+				.directionTo(Utils.getAvgArchonLocations(rc, getEnemyTeam())).opposite();
+		SHOULD_SPAWN_TANKS = Math.random() < TANK_SPAWNER_CHANCE
+				&& !(rc.getRobotCount() - 1 == rc.getInitialArchonLocations(rc.getTeam()).length);
 		moveDir = Utils.randomDirection();
-		towardsCenter = false;
-		settled = false;
 	}
 
 	@Override
@@ -27,64 +30,31 @@ public class GardenerLogic extends RobotLogic {
 
 		try {
 
-			if (rc.getRobotCount() - 1 == rc.getInitialArchonLocations(rc.getTeam()).length) {
-				tryAndBuildUnit(RobotType.SCOUT);
-				while (rc.getBuildCooldownTurns() != 0) {
-					Clock.yield();
-				}
-				tryAndBuildUnit(RobotType.SOLDIER);
-			}
+			buildInitialRoundsUnits();
 
-			int roundCount = 0;
-			Direction base = rc.getLocation().directionTo(Utils.getAvgArchonLocations(rc, getEnemyTeam()));
-
-			boolean shouldSpawnTanks = Math.random() > .5;
+			int numRoundsSettling = 0;
+			boolean settled = false;
 
 			while (true) {
-				if (!settled && roundCount > 30) {
-					settled = true;
-				} else if (!settled) {
-					roundCount++;
-					moveTowardsGoodSpot();
-					System.out.println("Moving");
-				} 
-				if(settled){
-					rc.setIndicatorLine(rc.getLocation(), rc.getLocation().add(base), 255, 255, 255);
-					if (rc.getBuildCooldownTurns() == 0) {
-						System.out.println("Should be able to spawn a unit");
-						Direction startAngle;
-						if (shouldSpawnTanks) {
-							startAngle = base.rotateLeftDegrees(90);
-						} else {
-							startAngle = base.rotateLeftDegrees(60);
-						}
 
-						System.out.println("Starting to try angles");
-						while (!rc.canPlantTree(startAngle) && Math.abs(base.degreesBetween(startAngle)) >= 50) {
-							startAngle = startAngle.rotateLeftDegrees(10);
-							System.out.println("Trying an angle of " + startAngle);
-						}
-						System.out.println("Done trying angles");
-						if (rc.canPlantTree(startAngle)) {
-							System.out.println("Trying to plant a tree");
-							rc.plantTree(startAngle);
-						} else {
-							System.out.println("Trying to spawn unit");
-							spawnUnit(base);
-						}
+				if(!settled) {
+					if (numRoundsSettling > NUM_ROUNDS_TO_SETTLE) {
+						settled = true;
+					} else {
+						numRoundsSettling++;
+						settled = moveTowardsGoodSpot();
 					}
+				} else {
+					createTreeRingAndSpawnUnits();
 				}
 
-				RobotInfo[] foes = rc.senseNearbyRobots(-1, getEnemyTeam());
-
-				if (foes.length > 0) {
-					BroadcastManager.saveLocation(rc, foes[0].location, LocationInfoType.GARDENER_HELP);
-				}
+				detectEnemiesAndSendHelpBroadcast();
 
 				waterLowestHealthTree();
 				tryAndShakeATree();
 				econWinIfPossible();
 				drawBullshitLine();
+
 				Clock.yield();
 			}
 
@@ -94,23 +64,41 @@ public class GardenerLogic extends RobotLogic {
 
 	}
 
-	private void moveBackAndForth(MapLocation center, Direction base) throws GameActionException {
-		if (towardsCenter) {
-			if (rc.canMove(base.opposite())) {
-				move(base.opposite());
-			}
-			if (rc.getLocation().distanceTo(center) < .25f) {
-				towardsCenter = false;
-			}
-		} else {
-			if (rc.canMove(base)) {
-				move(base);
+	private void createTreeRingAndSpawnUnits() throws GameActionException {
+		rc.setIndicatorLine(rc.getLocation(), rc.getLocation().add(ENEMY_BASE_OPPOSITE_DIRECTION), 255, 255, 255);
+		if (rc.getBuildCooldownTurns() == 0) {
+			System.out.println("Should be able to spawn a unit");
+			Direction startAngle;
+			if (SHOULD_SPAWN_TANKS) {
+				startAngle = ENEMY_BASE_OPPOSITE_DIRECTION.rotateLeftDegrees(90);
 			} else {
-				towardsCenter = true;
+				startAngle = ENEMY_BASE_OPPOSITE_DIRECTION.rotateLeftDegrees(60);
 			}
-			if (rc.getLocation().distanceTo(center) > 2) {
-				towardsCenter = true;
+
+			System.out.println("Starting to try angles");
+			while (!rc.canPlantTree(startAngle)
+					&& Math.abs(ENEMY_BASE_OPPOSITE_DIRECTION.degreesBetween(startAngle)) >= 50) {
+				startAngle = startAngle.rotateLeftDegrees(10);
+				System.out.println("Trying an angle of " + startAngle);
 			}
+			System.out.println("Done trying angles");
+			if (rc.canPlantTree(startAngle)) {
+				System.out.println("Trying to plant a tree");
+				rc.plantTree(startAngle);
+			} else {
+				System.out.println("Trying to spawn unit");
+				spawnUnit(ENEMY_BASE_OPPOSITE_DIRECTION);
+			}
+		}
+	}
+
+	private void buildInitialRoundsUnits() throws GameActionException {
+		if (rc.getRobotCount() - 1 == rc.getInitialArchonLocations(rc.getTeam()).length) {
+			tryAndBuildUnit(RobotType.SCOUT);
+			while (rc.getBuildCooldownTurns() != 0) {
+				Clock.yield();
+			}
+			tryAndBuildUnit(RobotType.SOLDIER);
 		}
 	}
 
@@ -141,40 +129,50 @@ public class GardenerLogic extends RobotLogic {
 		}
 	}
 
+	private RobotType determineUnitToSpawn(Direction intendedDirection) throws GameActionException {
+		if (BroadcastManager.getUnitCount(rc, UnitCountInfoType.ALLY_SCOUT) < 3) {
+			return RobotType.SCOUT;
+		} else if (rc.canBuildRobot(RobotType.TANK, intendedDirection)) {
+			return RobotType.TANK;
+		} else if (Math.random() > .8) {
+			return RobotType.LUMBERJACK;
+		} else {
+			return RobotType.SOLDIER;
+		}
+	}
+
 	private void spawnUnit(Direction direction) throws GameActionException {
 		if (rc.canBuildRobot(RobotType.LUMBERJACK, direction) && rc.getBuildCooldownTurns() == 0
 				&& rc.getTeamBullets() >= 100) {
 			System.out.println("Can build");
 			System.out.println(
 					"We have around " + BroadcastManager.getUnitCount(rc, UnitCountInfoType.ALLY_SCOUT) + "Scouts");
-			if (BroadcastManager.getUnitCount(rc, UnitCountInfoType.ALLY_SCOUT) < 3) {
-				rc.buildRobot(RobotType.SCOUT, direction);
-			} else if (rc.canBuildRobot(RobotType.TANK, direction)) {
-				rc.buildRobot(RobotType.TANK, direction);
-			} else if (Math.random() > .8) {
-				rc.buildRobot(RobotType.LUMBERJACK, direction);
-			} else {
-				rc.buildRobot(RobotType.SOLDIER, direction);
-			}
+			RobotType typeToBuild = determineUnitToSpawn(direction);
+			rc.buildRobot(typeToBuild, direction);
 		} else {
 			System.out.println("Can't build");
 		}
 	}
 
-	private void moveTowardsGoodSpot() throws GameActionException {
+	/*
+	 * Attempts to move to a good location. Returns true if a good location was
+	 * found after the move.
+	 */
+	private boolean moveTowardsGoodSpot() throws GameActionException {
 		// Try to find a free space to settle until 20 turns have elapsed
 		if (!isGoodLocation()) {
 			moveDir = moveWithRandomBounce(moveDir);
+			return false;
 		} else {
-			settled = true;
+			return true;
 		}
 	}
 
 	private boolean isGoodLocation() {
 		try {
 			// Check for free space of 3 radius - gives space to spawn trees
-			return !rc.isCircleOccupiedExceptByThisRobot(rc.getLocation().add(0, (float) .01), 3)
-					&& rc.onTheMap(rc.getLocation().add(0, (float) .01), 3);
+			return !rc.isCircleOccupiedExceptByThisRobot(rc.getLocation().add(0, (float) .01), MIN_FREE_SPACE_REQUIREMENT)
+					&& rc.onTheMap(rc.getLocation().add(0, (float) .01), MIN_FREE_SPACE_REQUIREMENT);
 		} catch (GameActionException e) {
 			return false;
 		}
